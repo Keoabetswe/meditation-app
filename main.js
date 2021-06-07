cfc02bca28614f05c79f28afa6e341155a5374e8
Keoabetswe <keo.nthite@iress.com>
2021-06-07 11:26:52 +0200

    declare app variables

---
+++ main.js
@@ -0,0 +1,18 @@
+const app = () => {
+    const sound = document.querySelector('.sound');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline');
+    const video = document.querySelector('.default-vid video');
+
+    //sounds
+    const selectedSound = document.querySelector('.select-sound button');
+
+    //time display
+    const timeDisplay = document.querySelector('.time-display');
+
+    //get outline length
+    const outlineLength = outline.getTotalLength();
+    
+}
+
+app();
